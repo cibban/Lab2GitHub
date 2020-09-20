af039fe75df6b7386fe878226f9a76ebf81adee6
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-20 21:26:02 +0200

    Fixade till output. Stänger filen efter läsning.

--- src/Labb2/FileCrawler.java
+++ src/Labb2/FileCrawler.java
@@ -1,24 +1,27 @@
 package Labb2;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class FileCrawler {
 
     // Initiera en global träffräknare.
+    // (Jag skapade den för debug egentligen men det är ju onödigt att ta bort den. //P)
     static int hitCount = 0;
 
     public static void main(String[] args) {
             //Initiera variabler
-            File initialPath = new File("");
+            File initialPath = new File("./");
             Scanner userInput = new Scanner(System.in);
             String searchString;
 
 
             // Be användaren om en söksträng.
             System.out.println("Välkommen till filKryparen!");
-            System.out.print("Skriv in ett sökord: ");
+            System.out.println();
+            System.out.print("Skriv ett sökord: ");
             searchString = userInput.nextLine();
             System.out.println("-------");
             System.out.println();
@@ -29,6 +32,8 @@ public class FileCrawler {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Skriv ut en tom rad och antal träffar.
         System.out.println();
         System.out.println(hitCount + " filer hittades.");
     }
@@ -54,10 +59,12 @@ public class FileCrawler {
                     fileScan.nextLine();
 
                     // Sök efter söksträngen i textraden.
-                    // Vid träff, skriv ut filens sökväg och uppdatera räknaren.
+                    // Vid träff, skriv ut filens sökväg, uppdatera räknaren
+                    // stäng filen och återvänd.
                     if(searchString.equals(fileScan.findInLine(searchString))) {
                         System.out.println(filePath.getCanonicalPath());
                         hitCount++;
+                        fileScan.close();
                         return;
                     }
                 }
@@ -68,7 +75,8 @@ public class FileCrawler {
             if(!filePath.canRead()) {
                 System.err.println("Fel: Kan inte läsa " + filePath.getCanonicalPath());
             }
-        } catch (Exception e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
