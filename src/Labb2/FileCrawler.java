939e6c97aa16c1ce8ee0dd2b9ebabf7cdbc7a44a
Patrik Sjöstrand <patrik.sjostrand@student.kyh.se>
2020-09-24 02:09:32 +0200

    Rättade buggar enligt feedback från Andreas.

--- src/Labb2/FileCrawler.java
+++ src/Labb2/FileCrawler.java
@@ -11,73 +11,67 @@ public class FileCrawler {
     // (Jag skapade den för debug egentligen men det är ju onödigt att ta bort den. //P)
     static int hitCount = 0;
 
-    public static void main(String[] args) {
-            //Initiera variabler
-            File initialPath = new File("./");
-            Scanner userInput = new Scanner(System.in);
-            String searchString;
+    public static void main(String[] args) throws IOException {
+        //Initiera variabler
+        File initialPath = new File("./");
+        Scanner userInput = new Scanner(System.in);
+        String searchString;
 
 
-            // Be användaren om en söksträng.
-            System.out.println("Välkommen till filKryparen!");
-            System.out.println();
-            System.out.print("Skriv ett sökord: ");
-            searchString = userInput.nextLine();
-            System.out.println("-------");
-            System.out.println();
+        // Be användaren om en söksträng.
+        System.out.println("Välkommen till filKryparen!");
+        System.out.println();
+        System.out.print("Skriv ett sökord: ");
+        searchString = userInput.nextLine();
+        System.out.println("-------");
+        System.out.println();
 
-        try {
-            // Påbörja sökningen.
-            crawlFileTree(initialPath, searchString);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Påbörja sökningen.
+        crawlFileTree(initialPath, searchString);
 
         // Skriv ut en tom rad och antal träffar.
         System.out.println();
         System.out.println(hitCount + " filer hittades.");
     }
 
-    public static void crawlFileTree(File filePath, String searchString) {
+    public static void crawlFileTree(File filePath, String searchString) throws IOException {
+        // Om filen/katalogen inte går att läsa, skriv felmeddelande till System.error.
+        if (!filePath.canRead()) System.err.println("Det går inte att läsa filen " + filePath.getCanonicalPath());
 
-        try {
-            // Om nästa sökväg i listan pekar på en katalog kallar funktionen
-            // på sig själv och fortsätter nedåt.
-            if (filePath.isDirectory()) {
 
-                File[] folderContents = filePath.listFiles();
+        // Om nästa sökväg i listan pekar på en katalog kallar funktionen
+        // på sig själv och fortsätter nedåt i filstrukturen.
+        if (filePath.isDirectory()) {
+
+            File[] folderContents = filePath.listFiles();
+            if (folderContents != null) {
                 for (File contentPath : folderContents) {
                     crawlFileTree(contentPath, searchString);
                 }
+            }
 
             // Om nästa sökväg i listan är en fil, öppna filen och sök
-            } else if (filePath.isFile()) {
-                Scanner fileScan = new Scanner(filePath);
+        } else if (filePath.isFile()) {
+            Scanner fileScan;
+                fileScan = new Scanner(filePath);
 
-                // Läs in nästa rad.
-                while(fileScan.hasNextLine()) {
-                    fileScan.nextLine();
+            // Läs in nästa rad.
+            while(fileScan.hasNextLine()) {
 
-                    // Sök efter söksträngen i textraden.
-                    // Vid träff, skriv ut filens sökväg, uppdatera räknaren
-                    // stäng filen och återvänd.
-                    if(searchString.equals(fileScan.findInLine(searchString))) {
+                // Vid träff, skriv ut filens sökväg och uppdatera räknaren
+                if(searchString.equals(fileScan.findInLine(searchString))) {
+                    try {
                         System.out.println(filePath.getCanonicalPath());
-                        hitCount++;
-                        fileScan.close();
-                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+                    hitCount++;
+                    return;
                 }
+                fileScan.nextLine();
             }
-
-            // Kontrollera om filen går att läsa.
-            // Om inte, skriv till System.error.
-            if(!filePath.canRead()) {
-                System.err.println("Fel: Kan inte läsa " + filePath.getCanonicalPath());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Stäng filen.
+            fileScan.close();
         }
     }
 }
